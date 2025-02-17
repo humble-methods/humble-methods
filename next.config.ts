2280bff5c1ec254e7e09ca3fc87c8733e5dd3cda
Hank Chen <cchen9299@gmail.com>
2025-02-16 21:05:40 -0700

    chore(*): remove gh-pages

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: true,
-  images: {
-    unoptimized: true, // Disable default image optimization
-  },
-  assetPrefix: isProd ? '/humble-methods/' : '',
-  basePath: isProd ? '/humble-methods' : '',
-  output: 'export'
 };
 
 export default nextConfig;
